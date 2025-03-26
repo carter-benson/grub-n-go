ea03273333e712e091895fa9ffd2a8fff2bb5b57
Carter Benson <145940670+carter-benson@users.noreply.github.com>
2025-03-26 01:50:11 +0000

    updated themes

--- main-app/src/App.tsx
+++ main-app/src/App.tsx
@@ -4,45 +4,51 @@ import ResponsiveAppBar from './components/ResponsiveAppBar.js';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme.js'
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
 
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          mt: 0,
-          mb: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Carter Benson
-        </Typography>
-
-        <Typography
+      <Container maxWidth="lg">
+        <Box
           sx={{
-            textAlign: 'center',
-            color: 'text.secondary',
-            width: { sm: '100%', md: '80%' },
+            mt: 0,
+            mb: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
-          Currently studying computer science at Mercer University, located in Macon, Georgia.
-        </Typography>
 
-        <ResponsiveAppBar />
+          <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+            Carter Benson
+          </Typography>
 
-        <div>
-          <Button variant="contained" color="primary">
-            Welcome to Grub N Go, home of the Grub Burger.
-          </Button>
-        </div>
-      </Box>
-    </Container>
+          <Typography
+            sx={{
+              textAlign: 'center',
+              color: 'text.secondary',
+              width: { sm: '100%', md: '80%' },
+            }}
+          >
+            Currently studying computer science at Mercer University, located in Macon, Georgia.
+          </Typography>
+
+          <ResponsiveAppBar />
+
+          <div>
+            <Button variant="contained" color="primary">
+              Welcome to Grub N Go, home of the Grub Burger.
+            </Button>
+          </div>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- main-app/src/theme.tsx
+++ main-app/src/theme.tsx
@@ -3,7 +3,36 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: '#1976d2', // primary color
+    },
+    secondary: {
+      main: '#acacac', // secondary color
+    },
+    background: {
+      default: '#ffffff', // background color
+      paper: '#ffffff', // paper background color
+    },
+    text: {
+      primary: '#000000', // text color
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          variants: [
+            {
+              props: { severity: 'info' },
+              style: {
+                backgroundColor: '#60a5fa',
+              },
+            },
+          ],
+        },
+      },
     },
   },
 });
